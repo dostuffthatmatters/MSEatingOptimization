3668c49376887625f22832cb891b48d195d99a1f
Moritz Makowski <makowskimoritz@gmail.com>
2019-10-29 11:02:50 +0100

    Initial commit

---
+++ Helpers/README.md
@@ -0,0 +1,7 @@
+## Custom Helper Classes
+
+Classes: `CustomPrinting`
+
+See some usage examples at the bottom of the each file.
+You can run it to see the result of these examples.
+
---
+++ Optimization/__init__.py
@@ -0,0 +1,12 @@
+
+class Optimization:
+
+    def __init__(self, input_file="Source/in.csv", output_file="Source/out.csv"):
+        self.input_file = input_file
+        self.output_file = output_file
+
+    def load_models(self):
+        pass
+
+    def execute(self):
+        pass
